1a68592a491a3f471d45b00cb70beaa1a9f1cde8
shen <shen@shendeMacBook.local>
2015-04-10 16:58:03 +0800

    Initial Commit

---
+++ Xiaoyuanershou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xiaoyuanershou
+//
+//  Created by shen on 15/4/10.
+//  Copyright (c) 2015年 Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
